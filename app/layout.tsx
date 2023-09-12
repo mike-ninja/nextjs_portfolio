e82642d932020657915f3e12d3136257b31e8e21
mike-ninja <mikeedubas@hotmail.com>
2023-09-12 18:38:04 +0300

    grid added

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,9 @@ export default function RootLayout({
         </div>
         <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[32.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]">
         </div>
+        <div className='main'>
+          <div className='gradient' />
+        </div>
 
         <ThemeContextProvider>
           <ActiveSectionContextProvider>
