55f4de630e9d0306e4855993340602e82fe4ec8e
mike-ninja <mikeedubas@hotmail.com>
2023-10-10 18:05:46 +0300

    updated border color for galaxy and theme switches

--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -17,7 +17,7 @@ export default function ThemeSwitch() {
         }
         toggleTheme();
       }}
-      className="fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950"
+      className="fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-gray-950/20 dark:border-white/40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950"
     >
       {theme === "light" ? <BsSun /> : <BsMoon />}
     </button>
